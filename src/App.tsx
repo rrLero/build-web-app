1928374ee4eb10e77e2ee1b306d55604a298bcc2
Roman Levchenko <roman.levchenko1981@gmail.com>
2021-11-05 22:03:08 +0200

    initial config

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.scss';
+import '../css/styles.scss';
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <h1 className="p-2 text-black text-4xl">Hello</h1>
+    );
+};
+
+export default App;
